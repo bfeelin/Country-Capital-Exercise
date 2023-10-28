8a82172dd52a6992c7278cf60b6e21eff16cc4f0
Brian Phelan <brianphelan59@gmail.com>
2023-10-28 11:54:39 -0400

    Render buttons, handle setting selected

--- src/CountryCapitalGame.tsx
+++ src/CountryCapitalGame.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 interface CountryCapitalMap {
   [country: string]: string;
@@ -14,10 +14,33 @@ const CountryCapitalGame: React.FC<Props> = ({ data }) => {
   const arrayData = countries.concat(cities).sort(() => 0.5 - Math.random())
 
   const [selectedButton, setSelectedButton] = React.useState<string | null>(null)
+  const [wrongPair, setWrongPair] = React.useState<Array<string> | null>(null)
+
+  function handleSetSelected(item: string) {
+    console.log(item)
+    if(selectedButton === null){
+        setSelectedButton(item)
+        setWrongPair(null)
+    }
+    else{
+        
+    }
+  }
 
   return (
     <>
-
+        {arrayData.map((item) => {
+            return(
+                <button 
+                    value={item}
+                    className={selectedButton === item ? "selected" :
+                                wrongPair?.includes(item) ? "wrong-pair" : ""}
+                    onClick={() => handleSetSelected(item)} 
+                    key={item}>
+                        {item}
+                </button>
+            )
+        })}
     </>
   );
 }
